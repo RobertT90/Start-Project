8166c4d983e10124f36712299a1b8f61645418d5
Robert <topolovec.r@gmail.com>
2020-03-26 10:39:25 +0100

    eddited production build webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -80,6 +81,11 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'styles.css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
         })
     ]
 }
